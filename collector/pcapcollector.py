ddfc374940eac4d6826461c4d360a63fdc14a693
nectostr <bastinda96@gmail.com>
2022-05-22 20:29:25 -0700

    first line reading no waiting case

--- collector/pcapcollector.py
+++ collector/pcapcollector.py
@@ -1,38 +1,51 @@
 """
 Module that set's up the pcap files data collection
 """
-from returns.result import Result, Success, Failure
-
 import subprocess
+import time
 from typing import Optional
 
+from returns.result import Result, Success, Failure
 
-def start_collecting(dump_folder: str, arguments: str) -> str:
+
+def start_collecting(dump_file: str, arguments: str) -> Result[str, str]:
     """
     Function that starts data collection
-    :param dump_folder: foldr to save data to
-    :param arguments: string with additional arguments for colelction
-    :return:
+    :param dump_file: foldr to save data to
+    :param arguments: string with additional arguments for collecting
+    :return: Failure with error or success with pid and filename
     """
-    proc = subprocess.Popen(f"tcpdump {arguments} -w {dump_folder}", shell=True)
-    return f"tcpdump started to '{dump_folder}' with <{proc.pid}>"
+    proc = subprocess.Popen(f"sudo tcpdump {arguments} -w {dump_file}",
+                            shell=True,
+                            universal_newlines=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    time.sleep(1)
+    out = next(iter(proc.stdout.readline, b''))
+    if out and ("tcpdump: listening" not in out):
+        return Failure(f"{out}")
+    return Success(f"{out} with <{proc.pid}>")
 
-def stop_collecting(pid: Optional[int]=None) -> Result[str, str]:
+
+def stop_collecting(pid: Optional[int] = None) -> Result[str, str]:
     """
     Kills either all tcpdump procc, or the one, that mentioned in agrs
     :param pid: process id
-    :return:
+    :return: Success or Failure with relevant info
     """
     if pid is None:
-        proc = subprocess.Popen("killall tcpdump", shell=True)
+        proc = subprocess.Popen("sudo killall tcpdump",
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
     else:
-        proc = subprocess.Popen(f"kill -9 {pid}", shell=True)
+        proc = subprocess.Popen(f"kill -9 {pid}",
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
     out, err = proc.communicate()
-    if not (out is None) or not (err is None):
-        return Failure(str(out) + str(err))
+    if out != b"" or err != b"":
+        return Failure(f"out: {out}, err: {err}")
     else:
         return Success("Done stopping tcpdump")
-
-
-
-
